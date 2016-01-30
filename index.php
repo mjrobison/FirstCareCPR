21b388f10490f16bc4dc4e79718b31dc7c800176
Matthew Robison <Matthew Robison>
2016-01-30 13:39:19 -0600

    Creating first stage MVC look.

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php require_once('header.html'); ?>
+<main>
+    <div class="bg-photo hero">
+    </div>
+    <section class="training">
+        <h1>Training</h1>
+        <p>We instruct on the Life saving methods of CPR, AED, & First Aid. Certification is for two years. We teach these classes for the professional and for the average person just wanting to know how to help in an emergency. </p>
+        <p>We teach First Aid, CPR, AED, & Blood Born Pathogens. We have regular classes scheduled throughout the week and occasionally on Saturday morning.  We are willing to travel to businesses or homes to accommodate our students.</p>
+    </section>
+    <section class="available">
+        <h2> Available Courses:</h2>
+        <article>
+            <h3>First Aid</h3>
+            <p>The First Aid course is designed for the responder. This class teaches the skills necessary to overcome reluctance to act in emergency situations and to recognize and care for other emergencies such as Shock, Bleeding, Broken Bones, & other situations. Designed for: Everyday Citizens (Employees; Parents; Teachers; Coaches; Babysitters and other Care Givers) Certification: Two (2) Years Class Length: 4 Hours</p>
+        </article>
+        <article>
+            <h3>CPR and AED</h3>
+            <p>This course is designed to...Infant Child and Adult</p>
+            <p>The CPR/AED course is designed for the responder. This class teaches the skills necessary to overcome reluctance to act in emergency situations and to recognize and care for life-threatening respiratory or cardiac emergencies. Through “hands-on” training, students will learn the standard protocols for administering proper CPR techniques and understand when and how to use an automated external defibrillator (AED). Designed for: Everyday Citizens (Employees; Parents; Teachers; Coaches; Babysitters and other Care Givers). Certification: Two (2) Years Class Length: 4 Hours</p>
+        </article>
+        <article>
+            <h3>Blood Born Pathogens</h3>
+        </article>			
+    </section>
+<?php require_once('footer.html') ?>
