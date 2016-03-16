e3cad8a46c7066a59a5c9f4587becee8f97b9021
Matthew Robison <Matthew Robison>
2016-03-16 18:24:23 -0500

    SQL Injection protection.

--- formmailer.php
+++ formmailer.php
@@ -6,15 +6,15 @@ $phone = $_POST['phone'];
 $date = date("m-d-Y", strtotime($_POST['date']));
 $location = $_POST['location'];
 $class = $_POST['class_needed'];
-$formcontent= "From: $name \n Phone: $phone \n Call Back: $call \n Location Requested: $location \n Date(s) Requested: $date \n Class(es) Requested:"; 
+$formcontent= "From: $name \r\n Phone: $phone \r\n Call Back: $call \r\n Location Requested: $location \r\n Date(s) Requested: $date \r\n Class(es) Requested:"; 
 for ($i=0; $i < count($class); $i++)
 { 
     $formcontent .= $class[$i] . '\n';
 }
 $recipient = "baseballrocks0412@gmail.com";
-//$subject = "New Client Registration";
+$subject = "New Client Registration";
 //$mailheader = "From: $email \r\n";
-//mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+mail($recipient, $subject, $formcontent) or die("Error!");
 header("Location:https://squareup.com/market/first-care-cpr/cpr-class-registration-fee?square_lead=item_embed");
 require_once('footer.html');
 //window.open("", "_blank");
