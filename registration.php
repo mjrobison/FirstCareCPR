f758735990edd5c8dabf4faa51b0c49aa93d4d32
Matthew Robison <Matthew Robison>
2016-05-07 23:21:14 -0500

    registration validations

--- registration.php
+++ registration.php
@@ -7,18 +7,27 @@
             </p>
         
             <form action="formmailer.php" method="POST">
-                <p><label>Name/Group: </label><input type="text" name="name"></p>
-                <p><label>Email: </label><input type="text" name="email"></p>
-                <p><label>Phone: </label><input type="text" name="phone"></p> 
-                <p><label>Date: </label> <input type="date" name="date"></input></p>
+                <p><label>Name/Group: </label><input type="text" name="name" placeholder="John Doe" required></p>
+                <p><label>Email: </label><input type="email" name="email" required placeholder="john_doe@example.com"></p>
+                <p><label>Phone: </label><input type="phone" name="phone" required placeholder="555-555-5555" pattern="^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$"></p> 
+                <p><label>Date: </label> <input type="date" name="date" required></input></p>
                 <p><label>Location: </label> <input type="text" name="location"></p>
-                <p><label>Class(es) Needed: </label><br />
-                    <input type="checkbox" name="class_needed[]" value="CPR" />CPR<br />
-                    <input type="checkbox" name="class_needed[]" value="AED" />AED<br />
-                    <input type="checkbox" name="class_needed[]" value="FirstAidCertification" />First Aid Certification<br />
-                    <input type="checkbox" name="class_needed[]" value="WildernessTraining" />Wilderness Training<br />
+                <p><label>Class(es) Needed: </label><br /><br />
+                    <input type="checkbox" name="class_needed[]" value="CPR" required />CPR<br />
+                    <input type="checkbox" name="class_needed[]" value="AED" required />AED<br />
+                    <input type="checkbox" name="class_needed[]" value="FirstAidCertification" required />First Aid Certification<br />
+                    <input type="checkbox" name="class_needed[]" value="WildernessTraining" required />Wilderness Training<br />
                     
-                <input type="Submit"></input>
+                <button class="submit">Submit Registration</button>
+                </form>
+                <script>
+                    var checkboxes = $("input[type='checkbox']"),
+                    submitButt = $("button.submit");
+
+                    checkboxes.click(function() {
+                        submitButt.attr("disabled", !checkboxes.is(":checked"));
+                    });  
+                </script>
             </form>	
         </div>
     </main>
