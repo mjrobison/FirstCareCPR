eb6003210e6ce8a2b03369253984ac27bf96215c
Matthew Robison <Matthew Robison>
2016-05-12 13:44:53 -0500

    Added wordpress directory, so i could download it on my work computer

--- builds/development/404.php
+++
@@ -1,8 +0,0 @@
-<?php get_header(); ?>
-<?php require_once('header.html'); ?>
-	<main>
-		<p>I'm sorry the page youare looking for was not found.</p>
-		
-	</main>
-<?php require_once('footer.html'); ?>
-<?php get_footer(); ?>
